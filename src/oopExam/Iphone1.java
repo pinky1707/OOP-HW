c59a0cf45fd6c61273be3a948aee4a223ed3bab4
TofaelKabir <tofaelkabir@gmail.com>
2022-06-13 23:16:17 -0400

    OOP Exam

---
+++ src/oopExam/Iphone1.java
@@ -0,0 +1,35 @@
+package oopExam;
+
+/*
+what is the relation between regular class, Abstract class and Interface? Can you make a relation of Iphone1 with Phone Interface and AppleWatch Abstract class. [Complete the code after answering above]. [points: 30] . 
+
+ */
+
+public class Iphone1 {
+
+	/*
+	 * Assume 4 private variables here -- price (as int), Info (as String, write few
+	 * words when you bought it), user (as a char, M or F), madeInUSA (as boolean)
+	 * inside Iphone1 class. If the variables are private how can you use those
+	 * variable? What kind of concept you can use? use it just after the variables.
+	 * get the out come in TestPhone class. Complete this action and go to next.
+	 */
+	
+	/*
+	 * 07) Again, here in Iphone1, we got 4 private variables we know. Create 2
+	 * constructors inside it using that 4 private variables -- default and
+	 * parameterized after those variables (obviously before methods and after
+	 * variables), now go to Iphone2 class.
+	 */
+	
+	/*
+	 * Create a method - regularClassInfo and print out all the features of a
+	 * regular class compared with Interface and Abstract class you know.
+	 */
+
+	/*
+	 * Write a method inside them related to one of the features like youtube and
+	 * print out related info.
+	 */
+
+}
---
+++ src/oopExam/Iphone6.java
@@ -0,0 +1,31 @@
+package oopExam;
+
+/*
+In 2004, Iphone6 come into the market which inherits Iphone5. Implement it. 
+*/
+public class Iphone6 {
+	/*
+	 * Write a method inside them related to one of the features like compass and
+	 * print out related info. What kind of concept you can use to relate to Iphone1
+	 * to 6? Instantiate Iphone6 in TestPhone and show how many methods they can
+	 * call in TestPhone. [points: 70] . Comment out at the end of TestPhone to
+	 * inform me which is hierarchical inheritance, multilevel inheritance, and
+	 * single inheritance. [points: 50].
+	 */
+
+	/*
+	 * Now, Think about Iphone6. It also has 5 methods with the same name as
+	 * materials(). For example -- They are parameterized method (int ramPrice, int
+	 * cameraPrice), another one (int ramPrice, int cameraPrice, String
+	 * ProcessorPrice), etc. Can you use the void, parameterized, final, or static
+	 * method here as the same method name? if yes, create them and take more int
+	 * variable of your own. What kind of concept you can use? [points: 70].
+	 * Instantiate the class in TestPhone and initialize those methods. [points:
+	 * 70]. Now think about a regular class Name ConfiguredIphone6 which extends
+	 * Iphone6. Override those methods and change the logic at your own to show the
+	 * changes. [points: 50]. Show the output in TestPhone (how many methods is
+	 * possible to extends). [points: 50]. Paste the github link for the package
+	 * below
+	 * 
+	 */
+}
